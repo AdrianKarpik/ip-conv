7bee7f799d58b047e0476dc02d0e9dce30bc1ad5
Adrian-Karpik <80511356+AdrianKarpik@users.noreply.github.com>
2021-05-06 22:22:20 +0200

    Update ipConv.js

--- ipConv.js
+++ ipConv.js
@@ -1,77 +1,78 @@
-// Konfiguracja pól oraz wezłów (ID>zmiennych)
 const ins = document.querySelector("#in");
 const out = document.querySelector("#out");
 const msg = document.querySelector("#msgs");
+let pole = document.getElementById("spr");
+let poleK = document.getElementById("klasa");
+let btn = document.getElementById("sprBtn");
 const cls = {
-	a: {from:"00000000", to:"01111110"},
-	b: {form:"10000000", to:"10111111"},
-	c: {from:"11100000", to:"11011111"},
-	d: {from:"11100000", to:"11101111"},
-	e: {form:"11110000", to:"11111111"}
-}; //zakres klas
+    a: { from:"00000000", to:"01111110" },
+    b: { from:"10000000", to:"10111111" },
+    c: { from:"11000000", to:"11011111" },
+    d: { from:"11100000", to:"11101111" },
+    e: { from:"11110000", to:"11111111" }
+}
 
-let setMsg = (type, text="") =>{
-	if(type==='clear')
-		msg.innerHTML = "";
-	if(type==="error")
+let setMsg = (type, text) => {
+	if (type == "info")
+		msg.innerHTML = '<p>'+text+'</p>';
+	if (type == "error")
 		msg.innerHTML = '<p style="color: red">'+text+'</p>';
-	if (type =="info")
-		msg.innerHTML = "<p>"+text+"</p>";
-}
-//Usuelnienie brakujacych znakow dla formatu
-let strPad = (len, text,sign="0") => {
-	return sign.repeat(8-len)+""+text;
+	if (type == "clean")
+		msg.innerHTML = "";
 }
 
-let checkClass = (toCheck) =>{
+let strPad = (len, text, sign="0") => {
+	return sign.repeat(8-len) + text;
+}
+
+let checkClass = (toCheck)=>{
 	var oct = (toCheck >>> 0).toString(2);
-	for (let [key,val] of Object.entries(cls)){
-		if(val.from <= oct && val.to >= oct){
-			setMsg("info","Klasa: "+key);
+	for( let [key,val] of Object.entries(cls)){
+		if( val.from <= oct && val.to >= oct){
+			setMsg('info',"Klasa: "+key);
 			break;
 		}
 	}
 }
 
-//detekcja formatu adresu: dec2bin / bin2dec
 let detect = (e) => {
 	let val = e.target.value;
-	let oct = val.split('.');
-	if(oct[0].length+oct[1].length+oct[2].length+oct[3].length !== 32){
+	let oct = val.split(".");
+	if (oct[0].length+oct[1].length+oct[2].length+oct[3].length !== 32) {
 		dec2bin(val);
 	} else {
 		bin2dec(val);
 	}
 }
 
-
-//konwersja decimal>binary
 let dec2bin = (ip) => {
 	let binOct = "";
 	let oct = ip.split(".");
 	let oLen = oct.length;
-	if(oLen<4)
-			setMsg("error", "podano tylko "+oLen+" oktety");
+	if (oLen < 4)
+		setMsg("error", "podano tylko "+oLen+" oktety");
 	else
-		setMsg("clear");
-	for(i=0; i<oLen; i++){
-		var digit = parseInt(oct[i]);
+		setMsg("clean");
+
+	for (i = 0; i < oLen; i++) {
+		let digit = parseInt(oct[i]);
 		(i===0) ? checkClass(digit) : null;
 		binOct += (binOct === "" ? "" : "." );
-		if( digit >= 0 && digit <= 255){
+
+		if (digit >= 0 && digit <= 255) {
 			let bin = (digit >>> 0).toString(2);
 			let bLen = bin.length;
 			binOct += (bLen < 8) ? strPad(bLen, bin) : bin;
-		}else
-			setMsg("error", "Wartosc poza zakresem w oktecie" +i);
-		
+		} else {
+			setMsg("error", "wartosc poza zakresem w oktescie "+i);
+		}
 	}
-	//bin = (ip >>> 0).toString(2);
+
 	out.textContent = binOct;
 }
-//konwersja binary>decimal
+
 let bin2dec = (bin) => {
-		let dec = "";
+	let dec = "";
 	let oct = bin.split(".");
 
 	for (i = 0; i < oct.length; i++) {
@@ -81,5 +82,36 @@ let bin2dec = (bin) => {
 	out.textContent = dec;
 }
 
-//nasłuch zdarzeń
-ins.addEventListener('focusout', detect,false);
+function spr() {
+	console.log("dzialam");
+	console.log(pole.value, poleK.value);
+
+	let ip = pole.value;
+	let klasa = poleK.value;
+	let oct = ip.split(".");
+	let oLen = oct.length;
+	if (oLen < 4)
+		setMsg("error", "podano tylko "+oLen+" oktety");
+	else
+		setMsg("clean");
+
+	for (i = 0; i < oLen; i++) {
+		let digit = parseInt(oct[i]);
+		(i===0) ? checkClass(digit) : null;
+	}
+	console.log(msg.textContent.slice(7));
+
+	if(msg.textContent.slice(7) === klasa) {
+		setMsg("info", "Dobrze!");
+	}
+	else{
+		setMsg("info", "Źle, spróbuj jeszcze raz :(");
+	}
+	
+
+
+
+}
+
+ins.addEventListener("focusout", detect, false);
+btn.addEventListener('click', spr, false);
